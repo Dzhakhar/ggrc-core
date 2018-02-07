20e0ae3dbeaaf3cacbc2549892f1b968894c76f8
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2018-02-07 16:58:08 +0100

    Add auto logic to divide code PO for create and edit modals

--- test/selenium/src/lib/page/modal/base.py
+++ test/selenium/src/lib/page/modal/base.py
@@ -12,27 +12,32 @@ from lib.utils import selenium_utils
 class BaseModal(base.Modal):
   """Base class for creation and edition modals."""
   _locator_ui_title = locator.ModalCreateNewObject.UI_TITLE
-  _locator_ui_code = locator.ModalCreateNewObject.UI_CODE
+  _locator_code = locator.ModalCreateNewObject.CODE
   _locator_button_save = locator.ModalCreateNewObject.BUTTON_SAVE_AND_CLOSE
 
   def __init__(self, driver):
     super(BaseModal, self).__init__(driver)
-    self.button_save_and_close = base.Button(driver, self._locator_button_save)
+    # todo: add proper class model for create, edit modals
+    self.modal_type_lbl = base.Label(
+        self._driver, locator.ModalCommonConfirmAction.MODAL_TITLE)
+    self.is_create_modal = "New " in self.modal_type_lbl.text
+    self.button_save_and_close = base.Button(
+        self._driver, self._locator_button_save)
     self.ui_title = base.TextInputField(self._driver, self._locator_ui_title)
-    self.ui_code = base.TextInputField(self._driver, self._locator_ui_code)
+    self.code = (
+        base.TextInputField(self._driver, self._locator_code) if
+        self.is_create_modal
+        else base.Label(self._driver, self._locator_code))
 
   def enter_title(self, text):
     """Enter title to modal."""
     self.ui_title.enter_text(text)
 
-  def enter_code(self, code):
-    """Enter code to modal."""
-    self.ui_code.enter_text(code)
-
   def fill_minimal_data(self, title, code):
     """Enter common minimal data to modal."""
     self.enter_title(title)
-    self.enter_code(code)
+    if self.is_create_modal:
+      self.code.enter_text(code)
     return self.__class__(self._driver)
 
   def edit_minimal_data(self, title):
@@ -52,7 +57,6 @@ class ProgramsModal(BaseModal):
     self.ui_description = base.Iframe(
         self._driver, self._locators.UI_DESCRIPTION)
     self.ui_notes = base.Iframe(self._driver, self._locators.UI_NOTES)
-    self.ui_code = base.TextInputField(self._driver, self._locators.UI_CODE)
     self.ui_state = base.Dropdown(self._driver, self._locators.UI_STATE)
     self.ui_show_optional_fields = base.Toggle(
         self._driver, self._locators.BUTTON_SHOW_ALL_OPTIONAL_FIELDS)
@@ -77,12 +81,6 @@ class ProgramsModal(BaseModal):
     Args: notes basestring)
     """
     self.ui_notes.find_iframe_and_enter_data(notes)
-
-  def enter_code(self, code):
-    """Enter text into code element.
-    Args: code (basestring)
-    """
-    self.ui_code.enter_text(code)
 
   def select_state(self, state):
     """Selects state from dropdown."""
@@ -119,7 +117,6 @@ class ControlsModal(BaseModal):
     self.description = base.Label(driver, self._locators.DESCRIPTION)
     self.test_plan = base.Label(driver, self._locators.TEST_PLAN)
     self.notes = base.Label(driver, self._locators.NOTES)
-    self.code = base.Label(driver, self._locators.CODE)
     self.kind_or_nature = base.Label(driver, self._locators.KIND_OR_NATURE)
     self.fraud_related = base.Label(driver, self._locators.FRAUD_RELATED)
     self.frequency = base.Label(driver, self._locators.FREQUENCY)
@@ -132,7 +129,6 @@ class ControlsModal(BaseModal):
     self.ui_description = base.Iframe(driver, self._locators.UI_DESCRIPTION)
     self.ui_test_plan = base.Iframe(driver, self._locators.UI_TEST_PLAN)
     self.ui_notes = base.Iframe(driver, self._locators.UI_NOTES)
-    self.ui_code = base.TextInputField(driver, self._locators.UI_CODE)
     # multi input fields
     self.ref_url = base.MultiInputField(
         driver, self._locators.REF_URL_CSS)
@@ -177,12 +173,6 @@ class ControlsModal(BaseModal):
     Args: text (basestring)
     """
     self.ui_notes.find_iframe_and_enter_data(text)
-
-  def enter_code(self, code):
-    """
-    Args: text (basestring)
-    """
-    self.ui_code.enter_text(code)
 
 
 class ObjectivesModal(BaseModal):
